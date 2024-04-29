34d3b1cd783dd346ceca7fa0bfe2d9a98ab541a6
PilotenChris <45621350+PilotenChris@users.noreply.github.com>
2024-04-29 21:30:08 +0200

    Created database
    If database is not there, then create the database and add default data

--- project.py
+++ project.py
@@ -1,4 +1,78 @@
+import os.path
+import sqlite3
+
+DBFILE: str = "roll_call.db"
+
+
 def main() -> None:
+    if not os.path.isfile(DBFILE):
+        create_database()
+
+    # Login or Create account
+    start()
+
+
+# Function to create the database with the required Tables and Fields
+# And inserts the required starting data
+def create_database() -> None:
+    con = sqlite3.connect(DBFILE)
+    cur = con.cursor()
+
+    # Create the table Account in the Database with required fields
+    cur.execute("CREATE TABLE Account(AccountId INTEGER NOT NULL UNIQUE," +
+                "AccountType TEXT NOT NULL UNIQUE," +
+                "PRIMARY KEY(AccountId AUTOINCREMENT))")
+
+    # Create the table User in the Database with required fields
+    cur.execute("CREATE TABLE User(Id INTEGER NOT NULL UNIQUE," +
+                "Name TEXT NOT NULL, Birth TEXT NOT NULL," +
+                "Email TEXT NOT NULL, UniEmail TEXT," +
+                "Password TEXT NOT NULL, Account INTEGER NOT NULL," +
+                "PRIMARY KEY(Id AUTOINCREMENT)," +
+                "FOREIGN KEY(Account) REFERENCES Account(AccountId))")
+
+    # Create the table Degrees in the Database with required fields
+    cur.execute("CREATE TABLE Degrees(DegreeId INTEGER NOT NULL UNIQUE," +
+                "DegreeName TEXT NOT NULL," +
+                "PRIMARY KEY(DegreeId AUTOINCREMENT))")
+
+    # Create the table Degree in the Database with required fields
+    cur.execute("CREATE TABLE Degree(UserId INTEGER NOT NULL," +
+                "DegreeId INTEGER NOT NULL," +
+                "FOREIGN KEY(UserId) REFERENCES User(Id)," +
+                "FOREIGN KEY(DegreeId) REFERENCES Degrees(DegreeId)," +
+                "PRIMARY KEY(UserId, DegreeId))")
+
+    # Create the table Course in the Database with required fields
+    cur.execute("CREATE TABLE Course(CourseId INTEGER NOT NULL UNIQUE," +
+                "CourseName TEXT NOT NULL," +
+                "PassingGrade INTEGER NOT NULL," +
+                "PRIMARY KEY(CourseId AUTOINCREMENT))")
+
+    # Create the table Grade in the Database with required fields
+    cur.execute("CREATE TABLE Grade(UserId INTEGER NOT NULL," +
+                "CourseId INTEGER NOT NULL," +
+                "Task INTEGER NOT NULL," +
+                "Grade INTEGER NOT NULL," +
+                "FOREIGN KEY(CourseId) REFERENCES Course(CourseId)," +
+                "FOREIGN KEY(UserId) REFERENCES User(Id)," +
+                "PRIMARY KEY(UserId, CourseId, Task))")
+
+    # Create the table Classes in the Database with required fields
+    cur.execute("CREATE TABLE Classes(UserId INTEGER NOT NULL," +
+                "Course INTEGER NOT NULL," +
+                "FOREIGN KEY(Course) REFERENCES Course(CourseId)," +
+                "FOREIGN KEY(UserId) REFERENCES User(Id)," +
+                "PRIMARY KEY(UserId, Course))")
+
+    # Insert the default data into table Account
+    cur.execute("INSERT INTO Account (AccountType) VALUES ('Student')," +
+                "('Teacher'), ('Admin')")
+    con.commit()
+    con.close()
+
+
+def start() -> None:
     ...
 
 
